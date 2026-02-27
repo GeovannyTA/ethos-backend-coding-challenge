25930feea4fef5ef4f34bca97c182227b7745c4f
Geovanny Araujo <trejogeovannyaraujo@gmail.com>
2026-02-27 13:02:49 -0600

    Correcion documentos

--- api/src/domain/repositories/UserRepository,.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "../entities/User";
-
-export interface UserRepository {
-    create(user: User): Promise<void>;
-    findAll(): Promise<User[]>;
-    findByEmail(email: string): Promise<User | null>;
-    update(user: User): Promise<void>;
-    delete(id: string): Promise<void>;
-}
--- api/src/user/userRouter.ts
+++
@@ -1,5 +0,0 @@
-import { Elysia } from "elysia";
-
-export const userRouter = new Elysia({prefix: "/user"})
-    .post("/", () => "Create user")
-    .get("/", () => "Get all users")
